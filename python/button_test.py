45edba4d9145097bcf8976475dd800cc1ee361ec
Kunal Botla <kunal@kunalbotla.com>
2022-05-11 11:52:41 -0400

    add state

--- python/button_test.py
+++ python/button_test.py
@@ -4,11 +4,18 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 open_status = False
+old_openstatus = False
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
-        print("Run commands")
+        print("Open")
         open_status = True
     if GPIO.input(10) == GPIO.LOW:
-        print("Stop commands")
+        print("Closed")
         open_status = False
+
+    if open_status == old_openstatus:
+        pass
+    else:
+        if open_status == True:
+            print("Run command")
