a9441e84a57f4bedc501cb94cd3bd9ff4af7db7e
Kunal Botla <kunal@kunalbotla.com>
2022-05-11 11:58:53 -0400

    maybe this will work

--- python/button_test.py
+++ python/button_test.py
@@ -6,18 +6,16 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 open_status = False
 old_open_status = False
 
-while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Open")
-        open_status = True
-        old_open_status = False
-    if GPIO.input(10) == GPIO.LOW:
-        print("Closed")
-        open_status = False
-        old_open_status = True
+if GPIO.input(10) == GPIO.HIGH:
+    print("Open")
+    open_status = True
+    old_open_status = False
+if GPIO.input(10) == GPIO.LOW:
+    print("Closed")
+    open_status = False
+    old_open_status = True
 
-    if open_status == old_open_status:
-        pass
-    else:
-        if open_status != old_open_status and open_status == True:
-            print("Run command")
+if open_status == old_open_status:
+    pass
+if open_status != old_open_status and open_status == True:
+    print("Run command")
