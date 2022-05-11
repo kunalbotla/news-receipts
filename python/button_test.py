958b698d3aabbbb277a3b88d6e0648700956ec53
Kunal Botla <kunal@kunalbotla.com>
2022-05-11 11:48:34 -0400

    add open status

--- python/button_test.py
+++ python/button_test.py
@@ -3,6 +3,12 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+open_status = False
+
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+        print("Run commands")
+        open_status = True
+    if GPIO.input(0) == GPIO.LOW:
+        print("Stop commands")
+        open_status = False
