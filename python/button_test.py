eb732952e2264e5fa1817566344975cb34dd0ec0
Kunal Botla <kunal@kunalbotla.com>
2022-05-11 11:55:23 -0400

    add old open status

--- python/button_test.py
+++ python/button_test.py
@@ -4,17 +4,19 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 open_status = False
-old_openstatus = False
+old_open_status = False
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
         print("Open")
         open_status = True
+        old_open_status = False
     if GPIO.input(10) == GPIO.LOW:
         print("Closed")
         open_status = False
+        old_open_status = True
 
-    if open_status == old_openstatus:
+    if open_status == old_open_status:
         pass
     else:
         if open_status == True:
