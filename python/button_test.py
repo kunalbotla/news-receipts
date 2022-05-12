5b9ce2b1716c124f95d25554c5dda70050178b79
Kunal Botla <kunal@kunalbotla.com>
2022-05-12 11:53:12 -0400

    print mode

--- python/button_test.py
+++ python/button_test.py
@@ -3,6 +3,7 @@ GPIO_PIN = 3
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+print(GPIO.getmode(GPIO.BOARD))
 GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
 
 
