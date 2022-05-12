b2618df078002798525fb01eb67da3d2b7d8570d
Kunal Botla <kunal@kunalbotla.com>
2022-05-12 11:51:08 -0400

    pin 3

--- python/button_test.py
+++ python/button_test.py
@@ -1,17 +1,20 @@
+GPIO_PIN = 3
+
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
+
 
 # Set default door closed status
-old_door_status = GPIO.input(3)
+old_door_status = GPIO.input(GPIO_PIN)
 open_status = False
 old_open_status = False
 
 while True:
 
     # Check if door is open
-    door_status = GPIO.input(3)
+    door_status = GPIO.input(GPIO_PIN)
 
     # Check if the status has changed
     if door_status != old_door_status:
@@ -19,7 +22,7 @@ while True:
         old_door_status = door_status
 
 
-        if door_status == GPIO.HIGH:
+        if door_status == GPIO(3).HIGH:
             print("Open; placeholder for printing")
             open_status = True
             old_open_status = False
