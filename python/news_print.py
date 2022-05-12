2421af017b9f20abe1edac13ca82f7b783420913
Kunal Botla <kunal@kunalbotla.com>
2022-05-12 10:16:48 -0400

    fix

--- python/news_print.py
+++ python/news_print.py
@@ -11,8 +11,6 @@ from random import randrange
 # button
 
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 # printer
