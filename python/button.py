47d4554766ed1fa5ce2e138ba3cdc5aa4150b2b2
Kunal Botla <kunal@kunalbotla.com>
2022-05-12 10:41:59 -0400

    fix

--- python/button.py
+++ python/button.py
@@ -1,6 +1,6 @@
 from distutils.util import execute
 import RPi.GPIO as GPIO
-from python.news_print import print_static_page # Import Raspberry Pi GPIO library
+from python import print_static_page # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
